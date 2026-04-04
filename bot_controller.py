375b5429bc4ba5d34dfb6b293fe0c4a880860f36
soulnightmare9086-cmyk <soulnightmare9086@gmail.com>
2026-04-04 18:52:54 +0530

    Update bot_controller.py

--- bot_controller.py
+++ bot_controller.py
@@ -26,7 +26,10 @@ DEFAULT_DURATION = 300  # 5 minutes (300 seconds)
 MAX_DURATION = 300
 
 # MongoDB
-MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://Soul:Soul9086@soul.e9ugjn8.mongodb.net/test?retryWrites=true&w=majority"")
+MONGO_URL = os.getenv(
+    "MONGO_URL",
+    "mongodb+srv://Soul:Soul9086@soul.e9ugjn8.mongodb.net/test?retryWrites=true&w=majority"
+)
 # ========================================
 
 ATTACK_METHODS = ["UDP", "TCP", "HTTP", "SYN", "ICMP"]
