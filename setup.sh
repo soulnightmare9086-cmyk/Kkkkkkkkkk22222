e013d949f77dafe0d81d266c9ec1a3689e722f1e
soulnightmare9086-cmyk <soulnightmare9086@gmail.com>
2026-04-05 09:38:43 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# C engine compile karna
-gcc net_driver.c -o sys_lib -lpthread
-
-# Sirf existing files ko permission dena
-chmod +x sys_lib setup.sh
-
-echo "✅ PRIMEXARMY Engine is ready."
